f8dba302edbd337a8711861de77e1a95efcb7274
Jose Rodriguez <jrodriguezballester@gmail.com>
2021-09-17 17:12:57 +0200

    Registro-Autentificacion

--- app/src/main/java/com/example/proyectosdam/AuthActivity.kt
+++ app/src/main/java/com/example/proyectosdam/AuthActivity.kt
@@ -1,11 +1,109 @@
 package com.example.proyectosdam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.proyectosdam.databinding.ActivityAuthBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        val view = binding.root
+       // setContentView(R.layout.activity_auth)
+        setContentView(view)
+
+        setup()
     }
+    private fun setup() {
+        binding.registerButton.setOnClickListener {
+          /////
+            Toast.makeText(
+                this,
+                "pulsado",
+                Toast.LENGTH_SHORT
+            ).show();
+            /////
+
+            var myEmail = binding.emailEditText.text.toString()
+            var myPassword = binding.passWordEditText.text.toString()
+            var recursos = Recursos()
+            if (recursos.isValidEmail(myEmail) && recursos.isValidPassword(myPassword)) {
+                //registrar
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(myEmail, myPassword)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            // Registro satisfactorio continuar app
+                            Toast.makeText(
+                                this,
+                                "Enhorabuena, registro satisfactorio",
+                                Toast.LENGTH_SHORT
+                            ).show();
+
+                            continuarApp()
+                        } else {
+                            // no se ha registrado
+                            recursos.mostrarAviso1(
+                                this,
+                                "Error de Autentificacion",
+                                "No has podido registrarte en la aplicacion, puede que ya estes registrado"
+                            )
+                        }
+                    }
+            } else {
+                // Correo o Contraseña no valido mostrar ventana
+                recursos.mostrarAviso1(this, "Aviso", "El correo o la contraseña son incorrentos")
+            }
+        }
+        binding.loginButton.setOnClickListener {
+            /////
+            Toast.makeText(
+                this,
+                "pulsado",
+                Toast.LENGTH_SHORT
+            ).show();
+            /////
+            continuarApp()
+        }
+        /*
+         loginButton.setOnClickListener {
+
+               var myEmail = emailEditText.text.toString()
+                    var myPassword = passWordEditText.text.toString()
+                    var recursos = Recursos()
+                    if (recursos.isValidEmail(myEmail) && recursos.isValidPassword(myPassword)) {
+                        //registrar
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(myEmail, myPassword)
+                            .addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    // login satisfactorio continuar app
+                                    continuarApp()
+                                } else {
+                                    // no se ha registrado
+                                    recursos.mostrarAviso1(
+                                        this,
+                                        "Error de Login",
+                                        "No has podido ingresar en la aplicacion, Comprueba tu email y contraseña"
+                                    )
+                                }
+                            }
+                    } else {
+                        // Correo o Contraseña no valido mostrar ventana
+                        recursos.mostrarAviso1(this, "Aviso", "El correo o la contraseña son incorrentos")
+                    }
+                }
+
+             */
+    }
+
+    public fun continuarApp() {
+     //   val intent = Intent(this, MainActivity::class.java)
+    //    startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/example/proyectosdam/Recursos.kt
@@ -0,0 +1,56 @@
+package com.example.proyectosdam
+
+import android.content.Context
+import android.content.DialogInterface
+import android.text.TextUtils
+import android.util.Patterns
+import androidx.appcompat.app.AlertDialog
+
+class Recursos {
+    // validaciones
+    public fun isValidEmail(email: String): Boolean {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    //^ # inicio-de-cadena
+    //(? =. * [0-9]) # un dígito debe aparecer al menos una vez
+    //(? =. * [a-z]) # una letra minúscula debe aparecer al menos una vez
+    //(? =. * [A-Z]) # una letra mayúscula debe aparecer al menos una vez
+    //(? =. * [@ # $% ^ & + =]) # un carácter especial debe aparecer al menos una vez que pueda reemplazarlo con sus caracteres especiales
+    // (? = \\ S + $) # no se permiten espacios en blanco en toda la cadena
+    //. {4,} # . cualquier caracter,{n} n lugares,{n,m} n=min, m=max
+    //$ # final de cadena
+    public fun isValidPassword(password: String): Boolean {
+        val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$"
+        val pattern = Regex(PASSWORD_PATTERN)
+        return pattern.containsMatchIn(password)
+    }
+
+    public fun mostrarAviso2(contex: Context) {
+
+        AlertDialog.Builder(contex)
+            .setTitle("Titulo del diálogo")
+            .setMessage("Contenido del diálogo.")
+            .setPositiveButton(android.R.string.ok,
+                DialogInterface.OnClickListener { dialog, which ->
+                    //botón OK pulsado
+                })
+            .setNegativeButton(android.R.string.cancel,
+                DialogInterface.OnClickListener { dialog, which ->
+                    //botón cancel pulsado
+                })
+            .show()
+
+    }
+
+    public fun mostrarAviso1(contex: Context, titulo: String, mensaje: String) {
+        AlertDialog.Builder(contex)
+            .setTitle(titulo)
+            .setMessage(mensaje)
+            .setPositiveButton(android.R.string.ok,
+                DialogInterface.OnClickListener { dialog, which ->
+                    //botón OK pulsado
+                })
+            .show()
+    }
+}
